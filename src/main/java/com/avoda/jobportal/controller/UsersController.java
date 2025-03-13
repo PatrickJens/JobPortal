e97c23dba0ba90785c44c2f73a2e1eacebb72c6b
Patrick Cannell <pcannell14@gmail.com>
2025-03-13 16:57:48 -0500

    services 01

---
+++ src/main/java/com/avoda/jobportal/controller/UsersController.java
@@ -0,0 +1,10 @@
+package com.avoda.jobportal.controller;
+
+import com.avoda.jobportal.services.UsersTypeService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UsersController {
+    private final UsersTypeService usersTypeService;
+
+}
---
+++ src/main/java/com/avoda/jobportal/repository/UsersTypeRepository.java
@@ -0,0 +1,7 @@
+package com.avoda.jobportal.repository;
+
+import com.avoda.jobportal.entity.UsersType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersTypeRepository extends JpaRepository<UsersType, Integer> {
+}
